b6ab36bd0c7a0fc751f0d5da88fcd7be54d22b2a
mikalaizh <mikalai.zh@gmail.com>
2015-12-05 19:45:19 +0300

    Update and rename freeQuoteMandatoryFields.java to FreeQuoteMandatoryFields.java

---
+++ src/test/java/tests/freeQuote/FreeQuoteMandatoryFields.java
@@ -0,0 +1,20 @@
+package tests.freeQuote;
+
+import org.junit.Test;
+import tests.BaseTest;
+import static org.junit.Assert.assertTrue;
+
+
+public class FreeQuoteMandatoryFields extends BaseTest {
+
+    @Test
+    public void sendOnlineRequest(){
+        driver.get(baseURL + "free-quote/");
+        freeQuotePage.mandatoryFieldCheck();
+        String textName = freeQuotePage.mandatoryFieldName();
+        String textEmail = freeQuotePage.mandatoryFieldEmail();
+        assertTrue(textName.contains("Enter your name"));
+        assertTrue(textEmail.contains("Enter your email"));
+    }
+}
+
